b17be00ba2fb4d9cd8ad82b5c45309812ca86791
Bulgakov Alexander <buls@yandex.ru>
2023-06-04 19:59:11 +0300

    feat(use): Opt, MapVal

--- expr/use/api.go
+++ expr/use/api.go
@@ -14,6 +14,17 @@ func If[T any](condition bool, tru T) When[T] {
 	return newWhen(condition, tru)
 }
 
+// Opt wraps optional result of a method/function
+func Opt[T any](element T, ok bool) When[T] {
+	return newWhen(ok, element)
+}
+
+// MapVal retrieves the map value for the specified key, 'key exists' marker and wraps them into a condition expression object
+func MapVal[M ~map[K]V, K comparable, V any](m M, key K) When[V] {
+	v, ok := m[key]
+	return newWhen(ok, v)
+}
+
 // IfGet is like If but aimed to use an getter function
 func IfGet[T any](condition bool, then func() T) When[T] {
 	return If(condition, evaluate(condition, then))
