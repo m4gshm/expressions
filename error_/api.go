d1137e6607bbbcccfdf1ea57be00bf1f9f819ff7
Bulgakov Alexander <buls@yandex.ru>
2023-06-05 22:01:43 +0300

    feat(error_): Zip of tuples

--- error_/api.go
+++ error_/api.go
@@ -2,6 +2,8 @@ package error_
 
 import (
 	"errors"
+
+	"github.com/m4gshm/expressions/expr/use"
 )
 
 func New(text string) error {
@@ -30,4 +32,19 @@ func Convert[From, To any](optional Tuple[From], converter func(From) To) Tuple[
 		to = converter(optional.val)
 	}
 	return Tuple[To]{to, err}
+}
+
+func Zip[F, S any](first Tuple[F], second Tuple[S]) Tuple2[F, S] {
+	var f F
+	var s S
+	err1 := first.err
+	err2 := second.err
+	err := use.If(err1 != nil, err1).Else(err2)
+	if err1 == nil {
+		f = first.val
+	}
+	if err2 == nil {
+		s = second.val
+	}
+	return Tuple2[F, S]{f, s, err}
 }
---
+++ error_/tuple2.go
@@ -0,0 +1,23 @@
+package error_
+
+type Tuple2[F, S any] struct {
+	first  F
+	second S
+	err    error
+}
+
+func (o Tuple2[F, S]) Get() (F, S, error) {
+	return o.first, o.second, o.err
+}
+
+func (o Tuple2[F, S]) Vals() (F, S) {
+	return o.first, o.second
+}
+
+func (o Tuple2[F, S]) Err() error {
+	return o.err
+}
+
+func (o Tuple2[F, S]) Ok() bool {
+	return o.err != nil
+}
